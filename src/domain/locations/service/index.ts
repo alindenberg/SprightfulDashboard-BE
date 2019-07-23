8132c913c2e0e30ddcc081370d7200f91c95b3cb
Austin Lindenberg <alindenberg8@gmail.com>
2019-07-23 09:55:23 -0400

    Skeleton function for validate billing_cycles. Need to figure out how to handle year on the dates

--- src/domain/locations/service/index.ts
+++ src/domain/locations/service/index.ts
@@ -51,9 +51,30 @@ export default class {
 
   updateLocationBillingCycles(location_id: string, billing_cycles: BillingCycle[]): Promise<boolean> {
     // TODO: validate billing cycles. (should be 12(?) non overlapping & continuous linking on end-to-start days)
+    // let error = this.validateBillingCycles(billing_cycles)
+    // if (error != null) {
+    //   throw error
+    // }
     return this.repository.updateLocationBillingCycles(location_id, billing_cycles)
   }
 
+  // Billing cycles should be contiguous non-overlapping and cover all 365 days of the year
+  private validateBillingCycles(billing_cycles: BillingCycle[]): Error {
+    for (let i = 0; i < billing_cycles.length; i++) {
+      // Validate billing cycle length
+      if (billing_cycles[i].start_date >= billing_cycles[i].end_date) {
+        return new Error(`Billing cycle must not have end date (${billing_cycles[i].end_date}) on same day or before the start date (${billing_cycles[i].start_date}).`)
+      }
+      // Validate first billing cycles start date is the day after the last billing cycle's end date
+      else if (i == 0 && billing_cycles[i].start_date != (billing_cycles[billing_cycles.length - 1].end_date + 1)) {
+        return new Error("Billing cycles must be contiguous, non-overlapping segments that cover a whole year")
+      }
+      // Validate billing cycle starts on day after the previous one ends
+      else if (i != 0 && billing_cycles[i].start_date != (billing_cycles[i - 1].end_date + 1)) {
+        return new Error("Billing cycles must be contiguous, non-overlapping segments that cover a whole year")
+      }
+    }
+  }
   private validateBank(bank: Bank): Error {
     if (
       bank.flat_rate_hours < 0 ||
