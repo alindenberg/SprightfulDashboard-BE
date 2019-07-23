d5e1936079bd6282721db356b91d9fc56b6f947a
Austin Lindenberg <alindenberg8@gmail.com>
2019-07-23 10:00:34 -0400

    PowerCompany resolver on Location object

--- src/domain/locations/graphql/types/resolvers.ts
+++ src/domain/locations/graphql/types/resolvers.ts
@@ -1,7 +1,8 @@
 import LocationService from '../../service'
+import PowerCompanyService from '../../../powerCompanys/service'
 
 const service = new LocationService()
-
+const powerCompanyService = new PowerCompanyService()
 export default {
   Location: {
     energy_info: async (obj: any, args: any, context: any) => {
@@ -9,6 +10,9 @@ export default {
     },
     savings_info: async (obj: any, args: any, context: any) => {
       return await service.getSavingsInfo()
+    },
+    power_company: async (obj: any, args: any, context: any) => {
+      return await powerCompanyService.getPowerCompany(obj.power_company_id)
     }
   }
 }
